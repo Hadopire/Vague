14664ddd9730975a2dc8e044b1ebcf72716ccc51
Nicolas Charret <hadopire.nc@gmail.com>
2018-12-18 17:01:31 +0100

    ResourceFile implementation for a directory

---
+++ Engine/Source/ResourceCache/Win32_ResourceDirectory.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "IResourceFile.h"
+#include "Resource.h"
+
+#include <Windows.h>
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+class ResourceDirectory : public IResourceFile
+{
+  public:
+    ResourceDirectory(const std::string _dirPath) { m_dirPath = _dirPath + "/"; }
+    virtual ~ResourceDirectory();
+
+    virtual bool Open();
+    virtual int GetRawResourceSize(const Resource &_resource);
+    virtual int GetRawResource(const Resource &_resource, char *_pBuffer);
+    virtual int GetNumResources() const;
+    virtual std::string GetResourceName(int _num) const;
+
+  private:
+    std::string m_dirPath;
+    std::unordered_map<std::string, std::pair<HANDLE, int>> m_dirTree;
+    std::vector<std::string> m_dirFiles;
+};
